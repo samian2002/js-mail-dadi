daf5c3623d4d3c0ed74c8898aec1dd08bc449a84
samian2002 <samiantariq14@gmail.com>
2024-04-24 14:00:15 +0200

    creo ciclo

--- mail/scripts/main.js
+++ mail/scripts/main.js
@@ -1,2 +1,14 @@
 //Lista mail
-const mailAutorizzate = ["mario@rossi.it", "giuseppe@verdi.it", "lucia@banfi.it"]
+const mailAutorizzate = ["mario@rossi.it", "giuseppe@verdi.it", "lucia@banfi.it"];
+//Richiesta mail
+let utenteMail = prompt("Inserisci la tua mail");
+//Controllo mail
+let = messaggio = "Accesso Negato"
+for (let i = 0; i < mailAutorizzate.length; i++) {
+    //controlla se la mail utente e autorizzata
+    if (mailAutorizzate[i] === utenteMail) {
+        messaggio = "Utente Autorizzato"
+        break
+    }
+}
+
