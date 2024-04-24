ba56ffa5c3d074027259c9c932ea796d7adf6b69
samian2002 <samiantariq14@gmail.com>
2024-04-24 14:08:17 +0200

    array

--- mail/scripts/main.js
+++ mail/scripts/main.js
@@ -3,12 +3,26 @@ const mailAutorizzate = ["mario@rossi.it", "giuseppe@verdi.it", "lucia@banfi.it"
 //Richiesta mail
 let utenteMail = prompt("Inserisci la tua mail");
 //Controllo mail
-let = messaggio = "Accesso Negato"
+
+//Ciclo
+/* 
+let messaggio = "Accesso Negato"
 for (let i = 0; i < mailAutorizzate.length; i++) {
     //controlla se la mail utente e autorizzata
     if (mailAutorizzate[i] === utenteMail) {
         messaggio = "Utente Autorizzato"
         break
     }
+}
+*/
+
+//Metodo Arrey
+let messaggio = "";
+if (mailAutorizzate.includes(utenteMail)) {
+    //Se utente autorizzato
+    messaggio = "Utente Autorizzato"
+} else {
+    //Se utente non autorizzato
+    messaggio = "Accesso Negato"
 }
 
