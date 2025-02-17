4b39e2e6305ac2e779aa0c2ac97286a31a60fc89
samian2002 <samiantariq14@gmail.com>
2025-02-17 05:13:35 +0100

    genero i numeri casuali

---
+++ dadi/main.js
@@ -0,0 +1,4 @@
+//Genero il numero casuale per l'utente
+const userNumber = Math.floor(Math.random() * 6) + 1;
+//Genere un numero casuale per il pc
+const pcNumber = Math.floor(Math.random() * 6) + 1;
