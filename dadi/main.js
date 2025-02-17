bc98fc74a690ba37796ccd5a46e6489a0253b989
samian2002 <samiantariq14@gmail.com>
2025-02-17 05:23:31 +0100

    confronto i numeri per stabilire il vincitore

--- dadi/main.js
+++ dadi/main.js
@@ -2,3 +2,12 @@
 const userNumber = Math.floor(Math.random() * 6) + 1;
 //Genere un numero casuale per il pc
 const pcNumber = Math.floor(Math.random() * 6) + 1;
+//Stampo i numeri generati
+console.log('Numero utente: ' + userNumber);
+console.log('Numero pc: ' + pcNumber);
+//Confronto i risultati per stabilire chi è il vincitore
+if (userNumber > pcNumber) {
+    alert('Hai vinto!');
+} else if (userNumber < pcNumber) {
+    alert('Hai perso!');
+}
