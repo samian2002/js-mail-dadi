e16af0d933e7809e56b2f7a27e78961b40254daa
samian2002 <samiantariq14@gmail.com>
2025-02-17 04:54:35 +0100

    first commit

---
+++ checkmail/main.js
@@ -0,0 +1,10 @@
+//Iniziallizzo array con le mail degli invitati
+const mailInvitati = [
+    'mario.rosso@gmail.com',
+    'valentina.gialli@libero.com',
+    'marco.polo@gmail.com',
+    'matteo.cattaneo@gmail.com',
+    'mario.balotelli@libero.it',
+    'clarissa.verdi@gmail.com',
+    'velentina.bianchi@yahoo.it'
+]
